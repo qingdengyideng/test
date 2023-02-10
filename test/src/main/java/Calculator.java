8670e98b6e3f44b2a06aaa477d1427cd71ea1d5b
liuhao <m@>
2023-02-10 14:35:10 +0800

    first commit.

--- test/src/main/java/Calculator.java
+++ test/src/main/java/Calculator.java
@@ -1,2 +1,91 @@
-package PACKAGE_NAME;public class Calculator {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    /**
+     * 计算后得到的结果集
+     */
+    private final List<BigDecimal> resultList = new ArrayList<>();
+
+    /**
+     * undo redo下标
+     */
+    private int resultIndex = -1;
+
+    /**
+     * 每次计算后得到结果值
+     */
+    private BigDecimal lastOperateNum;
+
+    public Calculator(String num) {
+        this.lastOperateNum = new BigDecimal(num);
+    }
+
+    public Calculator add(String num) {
+        lastOperateNum = lastOperateNum.add(new BigDecimal(num));
+        addToList();
+        return this;
+    }
+
+    public Calculator subtract(String num) {
+        lastOperateNum = lastOperateNum.subtract(new BigDecimal(num));
+        addToList();
+        return this;
+    }
+
+    public Calculator multiply(String num) {
+        lastOperateNum = lastOperateNum.multiply(new BigDecimal(num));
+        addToList();
+        return this;
+    }
+
+    public Calculator divide(String num) {
+        lastOperateNum = lastOperateNum.divide(new BigDecimal(num), 2, RoundingMode.HALF_UP);
+        addToList();
+        return this;
+    }
+
+    public Calculator divide(String num, int scale, RoundingMode roundingMode) {
+        lastOperateNum = lastOperateNum.divide(new BigDecimal(num), scale, roundingMode);
+        addToList();
+        return this;
+    }
+
+    private void addToList() {
+        resultList.add(lastOperateNum);
+        resultIndex++;
+    }
+
+    public BigDecimal getResult() {
+        return lastOperateNum;
+    }
+
+
+    public Calculator redo() {
+        if (resultIndex >= resultList.size() - 1) {
+            return this;
+        }
+        lastOperateNum = resultList.get(++resultIndex);
+        return this;
+    }
+
+    public Calculator undo() {
+
+        //只计算了一次undo为0
+        if (resultList.size() == 1) {
+            lastOperateNum = BigDecimal.ZERO;
+            return this;
+        }
+
+        if (resultIndex < 1) {
+            System.out.println("无法在undo了");
+            return this;
+        }
+
+        lastOperateNum = resultList.get(--resultIndex);
+        return this;
+    }
 }
--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BigDecimal result = new Calculator("3")
+                .add("5")   //3 + 5 = 8
+                .undo()     //第一次计算undo = 0
+                .add("6")   //0 + 6 = 6
+                .subtract("1")//6 - 1 = 5
+                .undo()     //undo = 6
+                .redo()     //redo = 5
+                .divide("2")//5 / 2 = 2.5
+                .undo()     //undo: 5
+                .undo()     //undo: 6
+                .getResult();
+        System.out.println(result);
+    }
 }
